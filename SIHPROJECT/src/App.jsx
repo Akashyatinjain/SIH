e1fcdf59e21651b60c2c4e8f49c50cbc822d5c21
Akash-Jain <aj0881871@gmail.com>
2025-09-20 21:29:33 +0530

    the changes of hospital and department

--- SIHPROJECT/src/App.jsx
+++ SIHPROJECT/src/App.jsx
@@ -2,16 +2,15 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "./Pages/HomePage.jsx";
 import SignIn from "./Pages/SignIn.jsx";
 import SignUp from "./Pages/SignUp.jsx";
-import Dashboard from "./Pages/dashboard.jsx"; // ✅ import dashboard
+import Dashboard from "./Pages/dashboard.jsx";
 import "./index.css";
-
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/signin" element={<SignIn />} />
       <Route path="/signup" element={<SignUp />} />
-      <Route path="/dashboard" element={<Dashboard />} /> {/* ✅ new route */}
+      <Route path="/dashboard" element={<Dashboard />} /> 
     </Routes>
   );
 }
--- SIHPROJECT/src/Pages/SignUp.jsx
+++ SIHPROJECT/src/Pages/SignUp.jsx
@@ -336,3 +336,4 @@ const SignUp = () => {
 };
 
 export default SignUp;
+
